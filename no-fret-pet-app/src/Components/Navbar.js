5d12d0e03f60e46d9b0222c495b000ae5d07729b
unknown <connorstack123@gmail.com>
2023-07-11 18:32:26 -0500

    5-navbar : basic nav

---
+++ no-fret-pet-app/src/Components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
