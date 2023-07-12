94fb8e85526099560eaf00bd8055ddc61afc24ad
unknown <connorstack123@gmail.com>
2023-07-11 19:09:00 -0500

    5-navbar : Basic navbar

--- no-fret-pet-app/src/Components/Navbar.js
+++ no-fret-pet-app/src/Components/Navbar.js
@@ -1,27 +1,16 @@
 import React from "react";
 
 function Navbar() {
-  return (
-    <div>
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            Link
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            Link
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            Link
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-}
 
+  return (
+  <nav className="nav">
+    <a href="/"><img className="webIcon" src={window.location.origin + '/img/cat-and-dog.png'} /></a>
+    <ul>
+      <li><a href="/myPets">My Pets</a></li>
+      <li><a href="/myProducts">My Products</a></li>
+      <li><a href="/"><img className="userIcon" src={window.location.origin + '/img/paw.png'} /></a></li>
+    </ul>
+  </nav>
+  )
+}
 export default Navbar;
